fe103af030835ef524a61e719226b24e14bd1896
Alex Bousso <alexbousso@gmail.com>
2015-06-23 23:33:49 +0300

    Fixed minor bugs in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,7 @@ function runCommand {
         return
     fi
 
-    echo "runCommand: Running ${command}"
+    #echo "runCommand: Running ${command}"
     ${command}
     retval=$?
     if [[ ${retval} -ne 0 ]]; then
@@ -24,22 +24,30 @@ function runCommand {
 }
 
 for test in $(ls compError*.cpp); do
-	res=$($COMPILE $test)
-	if [[ $res -eq 0 ]]; then
+	echo "Trying to compile $test ..."
+	$($COMPILE $test 2> /dev/null)
+	if [[ $? -eq 0 ]]; then
 		echo "TEST ERROR: $test passed but was supposed to fail!"
-		exit(1)
+		exit 1
 	fi
 done
+
+echo "Compiling OOP5.cpp ..."
 
 rm -f *.h.gch
 rm -f OOP5
 runCommand "$COMPILE OOP5.cpp -o OOP5"
 
 chmod +x OOP5
-runCommand "OOP5"
+
+echo "Running the compiled code of OOP5.cpp :"
+echo
+
+runCommand "./OOP5"
 
 rm -f OOP5
 
+echo
 echo
 echo "All tests passed successfully!"
 echo "This test has Super Cow powers."
